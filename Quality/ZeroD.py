4e8347641a2562ec9c32628fcf95359b808388f4
Dário Hachisu Hossoda <58784697+dariohhossoda@users.noreply.github.com>
2022-11-04 16:12:49 -0300

    Altera classe do modelo 0D

--- Quality/ZeroD.py
+++ Quality/ZeroD.py
@@ -3,61 +3,87 @@ class ZeroDimensional:
     Classe de Qualidade da Água em Reservatórios - Modelo 0D
     Zero-Dimensional
     """
-    def __init__(self, zero_dim_input, zero_dim_params, timestep, sim_count):
+    def __init__(self, volume, Qin, Qout, Cin, As, k, v, timestep):
+        """
+        TODO: Documentar
+        """
+        self.Input = ZeroDimensional.Input(volume,
+                                           Qin,
+                                           Qout,
+                                           Cin,
+                                           As)
+        self.SimCount = len(self.Input.Volume)
+        self.Output = ZeroDimensional.Output(self.SimCount)
+        self.Params = ZeroDimensional.Params(k, v)
         self.Timestep = timestep
-        self.SimCount = sim_count
-        self.Input = zero_dim_input
-        self.Output = ZeroDimensional.ZeroDOutput()
-        self.Params = zero_dim_params
-    
-    class ZeroDInput:
+        
+    class Output:
+        """
+        Saída do modelo 0D
+        
+        Cout - Concentração de saída após o término da simulação
+        """
+        def __init__(self, size):
+            self.ConcentrationOut = [0 for _ in range(size)]
+
+    class Input:
         """
         Inputs do modelo 0D
-        Volume
-        Vazão de montante
-        Vazão de jusante
-        Concentração de montante
-        Carga de montante
-        Área de contato
+        ---------------
+        Volume (V) - list
+            
+        Vazão de montante (Qin) - list
+        
+        Vazão de jusante (Qout) - list
+        
+        Concentração de montante (Cin) - list
+        
+        Carga de montante (Cin) - list
+        
+        Área de contato (As) - list
         """
-        def __init__(self, volume, inflow, outflow, concentration_in, q_load, contact_area):
+        def __init__(self,
+                     volume,
+                     inflow,
+                     outflow,
+                     concentration_in,
+                     contact_area):
+            
             self.Volume = volume
             self.Inflow = inflow
             self.Outflow = outflow
             self.ConcentrationIn =concentration_in
-            self.Load = q_load
             self.ContactArea = contact_area
             
-    class ZeroDParameters:
+    class Params:
         """
         Parâmetros do modelo 0D
         
-        Coecifiente de Reação
-        Velocidade de Assentamento
+        Coecifiente de Reação - k
+        Velocidade de Assentamento - v
         """
-        def __init__(self):
-            self.ReactionCoefficient
-            self.SettlingVelocity
-        
-    class ZeroDOutput:
-        """
-        Output do modelo 0D
-        
-        Concentração de jusante
-        """
-        def __init__(self):
-            self.ConcentrationOut = 0
+        def __init__(self, k, v):
+            self.ReactionCoefficient = k
+            self.SettlingVelocity = v
         
     
-    def SimulateQuality(self):
-        self.SimCount = len(self.Input.Volume())
+    def RunModel(self):
         _in = self.Input
         _out = self.Output
         _param = self.Params
+        dt = self.Timestep
         
-        _out.ConcentrationOut[0] = _in.Concentration[0]
+        _out.ConcentrationOut[0] = _in.ConcentrationIn[0]
         
+        # FIXME: Verificar equacionamento
         for i in range(1, self.SimCount):
-            _out.Concentration_out[i] = (_out.Concentration_out[i - 1] + (self.Timestep / _in.Volume[i]) * (_in.Inflow[i] * _in.Concentration_in[i] + _in.Load[i])) \
-                (1 + (self.Timestep / _in.Volume[i]) * \
-                    (_in.Outflow[i] + _param.ReactionCoefficient * _in.Volume[i] + _param.SettlingVelocity * _in.ContactArea[i] + ((_in.Volume[i] - _in.Volume[i - 1]) / self.Timestep)))
+            _out.ConcentrationOut[i] = (_out.ConcentrationOut[i - 1]
+                                        + dt / _in.Volume[i]
+                                        * _in.Inflow[i] * _in.ConcentrationIn[i]) \
+                                        / (1 + dt / _in.Volume[i]
+                                           * _in.Outflow[i]
+                                           + _param.ReactionCoefficient[i]
+                                           * _in.Volume[i]
+                                           + _param.SettlingVelocity[i]
+                                           + (_in.Volume[i]
+                                              - _in.Volume[i - 1]) / dt)

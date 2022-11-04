f8022689ea713c1ffa00f93dfd11c964da4e5d1d
Dario <58784697+dariohhossoda@users.noreply.github.com>
2022-11-04 18:41:31 -0300

    Adiciona scaffolding dos modelos de quantidade

---
+++ Quantity/Hydrodynamic/SaintVenant.py
@@ -0,0 +1,6 @@
+class SaintVenant:
+    def __init__(self):
+        raise NotImplementedError('Ainda não implementado!')
+
+    def RunModel():
+        raise NotImplementedError('Ainda não implementado!')

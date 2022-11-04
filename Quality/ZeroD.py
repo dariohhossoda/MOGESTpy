9111b2e20d917dd0235adedf7fb0b1c7f9b81b5b
Dário Hachisu Hossoda <58784697+dariohhossoda@users.noreply.github.com>
2022-11-04 16:55:40 -0300

    Corrigido cálculo de concentração

--- Quality/ZeroD.py
+++ Quality/ZeroD.py
@@ -16,11 +16,11 @@ class ZeroDimensional:
         self.Output = ZeroDimensional.Output(self.SimCount)
         self.Params = ZeroDimensional.Params(k, v)
         self.Timestep = timestep
-        
+
     class Output:
         """
         Saída do modelo 0D
-        
+
         Cout - Concentração de saída após o término da simulação
         """
         def __init__(self, size):
@@ -31,15 +31,15 @@ class ZeroDimensional:
         Inputs do modelo 0D
         ---------------
         Volume (V) - list
-            
+
         Vazão de montante (Qin) - list
-        
+
         Vazão de jusante (Qout) - list
-        
+
         Concentração de montante (Cin) - list
-        
+
         Carga de montante (Cin) - list
-        
+
         Área de contato (As) - list
         """
         def __init__(self,
@@ -48,42 +48,42 @@ class ZeroDimensional:
                      outflow,
                      concentration_in,
                      contact_area):
-            
+
             self.Volume = volume
             self.Inflow = inflow
             self.Outflow = outflow
-            self.ConcentrationIn =concentration_in
+            self.ConcentrationIn = concentration_in
             self.ContactArea = contact_area
-            
+
     class Params:
         """
         Parâmetros do modelo 0D
-        
+
         Coecifiente de Reação - k
         Velocidade de Assentamento - v
         """
         def __init__(self, k, v):
             self.ReactionCoefficient = k
             self.SettlingVelocity = v
-        
-    
+
     def RunModel(self):
         _in = self.Input
         _out = self.Output
         _param = self.Params
         dt = self.Timestep
-        
+
         _out.ConcentrationOut[0] = _in.ConcentrationIn[0]
-        
-        # FIXME: Verificar equacionamento
+
         for i in range(1, self.SimCount):
             _out.ConcentrationOut[i] = (_out.ConcentrationOut[i - 1]
                                         + dt / _in.Volume[i]
-                                        * _in.Inflow[i] * _in.ConcentrationIn[i]) \
-                                        / (1 + dt / _in.Volume[i]
-                                           * _in.Outflow[i]
-                                           + _param.ReactionCoefficient[i]
-                                           * _in.Volume[i]
-                                           + _param.SettlingVelocity[i]
-                                           + (_in.Volume[i]
-                                              - _in.Volume[i - 1]) / dt)
+                                        * _in.Inflow[i]
+                                        * _in.ConcentrationIn[i]
+                                        ) / (1 + dt / _in.Volume[i]
+                                             * (_in.Outflow[i]
+                                                + _param.ReactionCoefficient[i]
+                                                * _in.Volume[i]
+                                                + _param.SettlingVelocity[i]
+                                                * _in.ContactArea[i]
+                                                + (_in.Volume[i]
+                                                   - _in.Volume[i - 1]) / dt))

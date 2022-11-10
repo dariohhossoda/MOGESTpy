03f90336260b81bdfc06435e0124aef9493592ae
Dario <58784697+dariohhossoda@users.noreply.github.com>
2022-11-10 19:32:36 -0300

    Linting

--- Quality/BuWo.py
+++ Quality/BuWo.py
@@ -11,7 +11,7 @@ class BuildUpWashoff:
         Args:
             use_name (_type_): LandUse
             Bmax (_type_): Maximum Buildup, normalized (kg/km²)
-            Nb (_type_): Buildup Exponent  (-). Nb <= 1 
+            Nb (_type_): Buildup Exponent  (-). Nb <= 1
             Kb (_type_): Buildup rate constant.
             - Pow: kg/km²*d^-Nb;
             - Exp: d^-1;
@@ -29,58 +29,57 @@ class BuildUpWashoff:
         self.BuildUp = []
         self.EffectiveWashoff = []
         self.Aggregate = aggregate
-    
+
     # region tempo (time) das equações de BuildUp
     def TimeFromBuildUpPow(self, buildup):
         try:
             time = (buildup) ** (1 / self.Nb)
         except:
             time = 0
-            
+
         return time
-    
+
     def TimeFromBuildUpExp(self, buildup):
         try:
             time = -math.log(1 - buildup / self.Bmax) / self.Kb
         except:
             time = 0
-            
+
         return time
-    
+
     def TimeFromBuildUpSat(self, buildup):
         try:
             time = buildup * self.Kb / (self.Bmax - buildup)
         except:
             time = 0
-            
+
         return time
     # endregion
-    
+
     # region BuildUp
     def BuildUpPow(self, time):
         return math.min(self.Bmax, self.Kb * (time ** self.Nb))
-    
+
     def BuildUpExp(self, time):
         return self.Bmax * (1 - math.e ** (self.Kb * time))
-    
+
     def BuildUpSat(self, time):
         return self.Bmax * time / (self.Kb + time)
     # endregion
-    
+
     # region Washoff
     def WashoffExp(self, surface_flow, buildup_mass):
         return self.Kw * (surface_flow ** self.Nw) * buildup_mass
-    
+
     def WashoffRating(self, surface_flow, watershed_area):
         return self.Kw * (1000 * surface_flow * watershed_area) ** self.Nw
-    
+
     def WashoffEMC(self, surface_flow, watershed_area):
         return self.Kw * surface_flow * watershed_area
     # endregion
-    
+
     def Process(simulation):
         """
         simulation -> BuildUpWashoff
         """
         raise NotImplementedError('Ainda não implementado!')
-        

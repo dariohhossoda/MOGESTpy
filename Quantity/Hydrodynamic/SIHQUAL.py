084f57b9079f18a6e542dfa50aff3c8b0ad54753
Dário Hachisu Hossoda <58784697+dariohhossoda@users.noreply.github.com>
2022-11-25 13:42:00 -0300

    Adiciona lógica antiga

--- Quantity/Hydrodynamic/SIHQUAL.py
+++ Quantity/Hydrodynamic/SIHQUAL.py
@@ -3,7 +3,7 @@ import numpy as np
 
 
 # Ler dados -> pandas
-filename = '.xlsx'
+filename = 'SIHQUAL.xlsx'
 
 data_sheet = 'Data'
 param_sheet = 'Parameters'
@@ -11,18 +11,64 @@ param_sheet = 'Parameters'
 data_df = pd.read_excel(filename, sheet_name=data_sheet)
 param_df = pd.read_excel(filename, sheet_name=param_sheet)
 
+# Guardar em variáveis
+
 dx = param_df[0][1]
 dt = param_df[1][1]
 
-alpha = param_df[2][1]
-coef_D = param_df[3][1]
+tf = param_df[2][1]
 
+alpha = param_df[3][1]
+coef_D = param_df[4][1]
 
-# Guardar em variáveis
-# Inicializa variáves (vetores -> numpy)
-# y, v, QL, b, m, n, So, alpha, dt, dx, g
-# Interopolação dos dados -> diário:horário
-# Output das seções
+x = data_df['x']
+So = data_df['So']
+m = data_df['m']
+y1 = data_df['y1']
+v1 = data_df['v1']
+b = data_df['b']
+B1 = data_df['B1']
+A1 = data_df['A1']
+Rh1 = data_df['Rh1']
+Sf1 = data_df['Sf1']
+Q1 = data_df['Q1']
+n = data_df['n']
+
+Kd = data_df['Kd']
+Ks = data_df['Ks']
+c1 = data_df['c1']
+cqd = data_df['cqd']
+
+output_sections = param_df['output_sections']
+
+output_df = pd.DataFrame()
+
 # Loop numérico
+for sim_time in range(stop=tf, step=dt):
+    # region Contribuição Lateral
+    
+    # endregion Contribuição Lateral
+    
+    # region Módulo Hidrodinâmico
+    
+    # endregion Módulo Hidrodinâmico
+    
+    # region Módulo de Qualidade
+    
+    # endregion Módulo de Qualidade
 
+    # region Redefinição de Variáveis
+    
+    # endregion Redefinição de Variáveis
+    
+    # region Condição de Estabilidade
+    
+    # endregion Condição de Estabilidade
+    
+    # region Output
+    
+    # endregion Output
+    
+    pass
 
+output_df.to_excel('SIHQUAL_output.xlsx')

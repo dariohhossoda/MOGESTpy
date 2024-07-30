32f76a1842552452a08dd28dbaad90a8c152ddfa
Dário Hachisu Hossoda <58784697+dariohhossoda@users.noreply.github.com>
2024-07-30 07:15:57 -0300

    Add tests

--- tests/test_smap_new.py
+++ tests/test_smap_new.py
@@ -40,27 +40,36 @@ def test_smap2_transfer_functions2():
     """
     Tests Transfer Functions of SMAP2 class
     """
-    smap = SMAP2.Smap()
-    smap.Str = 165.060822610565
-    smap.Ad = 17800.0
-    smap.Crec = 20.0
-    smap.Tuin = 0.8
-    smap.Ebin = 204.25
-    smap.Capc = 40.0
-    smap.kkt = 120.0
-    smap.k2t = 7.97306669390268
-    smap.Ai = 4
+    smap = SMAP2.Smap(
+        Str=165.060822610565,
+        Ad=17800.0,
+        Crec=20.0,
+        Tuin=0.8,
+        Ebin=204.25,
+        Capc=40.0,
+        kkt=120.0,
+        k2t=7.97306669390268,
+        Ai=4.0
+    )
 
-    prec = 21.4017248169365
+    prec = 21.0
     etp = 2.4
-    Rsolo = 80.9265336890574
-    Es_ref = 2.982390340677
-    Er_ref = 2.982390340677
-    Tu = 0.490283111456380000
-    Es_ = smap.Es_calc(prec, smap.Ai, smap.Str, Rsolo)
 
-    assert abs(Es_ - Es_ref) < 1e-6
-    assert abs(smap.Er_calc(prec, etp, Es_, Tu) - Er_ref) < 1e-6
+    Rsolo = 80.9265337
+    Tu = 0.4902831
+
+    Es_ref = 2.8575867
+    Er_ref = 2.4
+
+    Es_calc = smap.Es_calc(prec, smap.Ai, smap.Str, Rsolo)
+    Er_calc = smap.Er_calc(prec, etp, Es_calc, Tu)
+
+    Rec_calc = smap.Rec_calc(smap.Crec, Tu, Rsolo, smap.Capc, smap.Str)
+    Rec_ref = 1.4612599
+
+    assert abs(Es_calc - Es_ref) < 1e-6, f'Es: {Es_calc} | {Es_ref}'
+    assert abs(Er_calc - Er_ref) < 1e-6, f'Er: {Er_calc} | {Er_ref}'
+    assert abs(Rec_calc - Rec_ref) < 1e-6, f'Rec: {Rec_calc} | {Rec_ref}'
 
 
 def test_smap2_reservoir_init():
@@ -77,17 +86,21 @@ def test_smap2_run_step():
     """
     Tests RunStep method of SMAP2 class
     """
-    smap = SMAP2.Smap()
-    smap.Str = 165.060822610565
-    smap.Ad = 17800.0
-    smap.Crec = 20.0
-    smap.Tuin = 0.8
-    smap.Ebin = 204.25
-    smap.Capc = 40.0
-    smap.kkt = 120.0
-    smap.k2t = 7.97306669390268
-    smap.Ai = 4.0
+    smap = SMAP2.Smap(
+        Str=165.060822610565,
+        Ad=17800.0,
+        Crec=20.0,
+        Tuin=0.8,
+        Ebin=204.25,
+        Capc=40.0,
+        kkt=120.0,
+        k2t=7.97306669390268,
+        Ai=4.0
+    )
 
+    smap.Tu = smap.Tu_calc(smap.Rsolo, smap.Str)
+    assert abs(smap.Tu -
+               0.8) < 1e-6, f"Tu: {smap.Tu}"
     assert abs(smap.RunStep(0, 2.4) - 204.250000) < 1e-6
 
 
@@ -95,17 +108,22 @@ def test_smap2_run_step_multiple():
     """
     Tests RunStep method of SMAP2 class
     """
-    smap = SMAP2.Smap()
-    smap.Str = 165
-    smap.Ad = 17800
-    smap.Crec = 20
-    smap.Tuin = 0.8
-    smap.Ebin = 204
-    smap.Capc = 40
-    smap.kkt = 120
-    smap.k2t = 7.97
+    smap = SMAP2.Smap(
+        Str=165,
+        Ad=17800.0,
+        Crec=20.0,
+        Tuin=0.8,
+        Ebin=204.25,
+        Capc=40.0,
+        kkt=120.0,
+        k2t=8,
+        Ai=4.0
+    )
 
-    assert round(smap.RunStep(0, 2.4), 6) != 0
-    # assert smap.RunStep(0, 2.4) - 216 < 1e-6
-    # assert smap.RunStep(0, 2.4) - 224 < 1e-6
-    # assert smap.RunStep(0, 2.4) - 224 < 1e-6
+    step_1 = smap.RunStep(0, 2.4)
+    step_2 = smap.RunStep(0, 2.4)
+    step_3 = smap.RunStep(0, 2.4)
+
+    assert abs(step_1 - 204.25) < 1e-6, f"Step 1: {step_1}"
+    assert abs(step_2 - 215.6038845) < 1e-6, f"Step 2: {step_2}"
+    assert abs(step_3 - 223.5623454) < 1e-6, f"Step 3: {step_3}"

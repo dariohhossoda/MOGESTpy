997d3be1fd33e3b5379b058420d8d2a4ca600b00
Dário Hachisu Hossoda <58784697+dariohhossoda@users.noreply.github.com>
2024-07-30 07:15:34 -0300

    Linting

--- mogestpy/quantity/Hydrological/Muskingum.py
+++ mogestpy/quantity/Hydrological/Muskingum.py
@@ -14,22 +14,25 @@ class Muskingum:
         - downstream (list): List of downstream values.
         """
         # Coefficients
-        C0 = (T - (2 * K * x)) / ((2 * K * (1 - x)) + T)
-        C1 = (T + (2 * K * x)) / ((2 * K * (1 - x)) + T)
-        C2 = ((2 * K * (1 - x)) - T) / ((2 * K * (1 - x)) + T)
+
+        c0 = (T - (2 * K * x)) / ((2 * K * (1 - x)) + T)
+        c1 = (T + (2 * K * x)) / ((2 * K * (1 - x)) + T)
+        c2 = ((2 * K * (1 - x)) - T) / ((2 * K * (1 - x)) + T)
 
         n = len(upstream)
         downstream = [0] * n
         # Initial downstream value is equal to upstream value
+
         downstream[0] = upstream[0]
         # Loop through from the second to the last entries
-        for i in range(1, n):
-            downstream[i] = C0 * upstream[i] + C1 * \
-                upstream[i - 1] + C2 * downstream[i - 1]
 
+        for i in range(1, n):
+            downstream[i] = (
+                c0 * upstream[i] + c1 * upstream[i - 1] + c2 * downstream[i - 1]
+            )
         return downstream
 
-    def DownstreamFORK(K, X, m, T, I):
+    def downstream_fork(K, X, m, T, inflow):
         """
         Non-linear Muskingum model (first-order)
         with fourth-order Runge-Kutta method (routing
@@ -43,40 +46,42 @@ class Muskingum:
         X (float): Muskingum weighting factor.
         m (float): Muskingum exponent.
         T (float): Time step.
-        I (list): Input hydrograph.
+        inflow (list): Input hydrograph.
 
         Returns:
         list: Output hydrograph.
         """
-        n = len(I)
-
-        # Outflow
-        O = [0] * n
-        # Initial value
-        O[0] = I[0]
-        # Storage
-        S = [0] * n
-
+        n = len(inflow)
+        outflow = [0] * n
+        outflow[0] = inflow[0]
+        s = [0] * n
         for i in range(n - 1):
-            # Current storage
-            S[i] = K * (X * I[i] + (1 - X) * O[i]) ** m
-            # Coefficients
-            k1 = (-1 / (1 - X)) * ((S[i] / K) ** (1 / m) - I[i])
-            k2 = (-1 / (1 - X)) * (((S[i] + 0.5 * T * k1) / K)
-                                   ** (1 / m) - 0.5 * (I[i] + I[i + 1]))
-            k3 = (-1 / (1 - X)) * (((S[i] + 0.5 * T * k2) / K)
-                                   ** (1 / m) - 0.5 * (I[i] + I[i + 1]))
-            k4 = (-1 / (1 - X)) * \
-                (((S[i] + 1.0 * T * k3) / K) ** (1 / m) - I[i + 1])
-            # Next storage
-            S[i + 1] = S[i] + T * (k1 + 2 * k2 + 2 * k3 + k4) / 6
-            # Next outflow
-            O[i + 1] = (1 / (1 - X)) * ((S[i + 1] / K)
-                                        ** (1 / m) - X * I[i + 1])
+            s[i] = K * (X * inflow[i] + (1 - X) * outflow[i]) ** m
+            k1 = -1 / (1 - X) * ((s[i] / K) ** (1 / m) - inflow[i])
+            k2 = (
+                -1
+                / (1 - X)
+                * (
+                    ((s[i] + 0.5 * T * k1) / K) ** (1 / m)
+                    - 0.5 * (inflow[i] + inflow[i + 1])
+                )
+            )
+            k3 = (
+                -1
+                / (1 - X)
+                * (
+                    ((s[i] + 0.5 * T * k2) / K) ** (1 / m)
+                    - 0.5 * (inflow[i] + inflow[i + 1])
+                )
+            )
+            k4 = -1 / (1 - X) * (((s[i] + 1.0 * T * k3) / K) ** (1 / m) - inflow[i + 1])
+            s[i + 1] = s[i] + T * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+            outflow[i + 1] = (
+                1 / (1 - X) * ((s[i + 1] / K) ** (1 / m) - X * inflow[i + 1])
+            )
+        return outflow
 
-        return O
-
-    def UpstreamFORK(K, X, m, T, O):
+    def upstream_fork(K, X, m, T, outflow):
         """
         Non-linear Muskingum model (first-order)
         with fourth-order Runge-Kutta method (routing
@@ -95,30 +100,34 @@ class Muskingum:
         Returns:
         list: List of upstream inflow values
         """
-        n = len(O)
-
-        # Inflow
-        I = [0] * n
-        # Initial value
-        I[n - 1] = O[n - 1]
-
-        # Storage, rate of change
-        S = [0] * n
-
+        n = len(outflow)
+        inflow = [0] * n
+        inflow[n - 1] = outflow[n - 1]
+        s = [0] * n
         for i in range(n - 1, 0, -1):
-            # Storage
-            S[i] = K * (X * I[i] + (1 - X) * O[i]) ** m
-            # Coefficients
-            k1 = (1 / X) * ((S[i] / K) ** (1 / m) - O[i])
-            k2 = (1 / X) * (((S[i] + 0.5 * T * k1) / K)
-                            ** (1 / m) - (0.5 * (O[i] + O[i - 1])))
-            k3 = (1 / X) * (((S[i] + 0.5 * T * k2) / K)
-                            ** (1 / m) - (0.5 * (O[i] + O[i - 1])))
-            k4 = (1 / X) * (((S[i] + 1.0 * T * k3) / K) ** (1 / m) - O[i - 1])
-            # Storage at t - 1 (previous step)
-            S[i - 1] = S[i] - T * (k1 + 2 * k2 + 2 * k3 + k4) / 6
-            # Inflow at t - 1 (previous step)
-            I[i - 1] = (1 / X) * ((S[i - 1] / K) ** (1 / m)) - \
-                ((1 - X) / X) * O[i - 1]
-
-        return I
+            s[i] = K * (X * inflow[i] + (1 - X) * outflow[i]) ** m
+            k1 = 1 / X * ((s[i] / K) ** (1 / m) - outflow[i])
+            k2 = (
+                1
+                / X
+                * (
+                    ((s[i] + 0.5 * T * k1) / K) ** (1 / m)
+                    - 0.5 * (outflow[i] + outflow[i - 1])
+                )
+            )
+            k3 = (
+                1
+                / X
+                * (
+                    ((s[i] + 0.5 * T * k2) / K) ** (1 / m)
+                    - 0.5 * (outflow[i] + outflow[i - 1])
+                )
+            )
+            k4 = 1 / X * (((s[i] + 1.0 * T * k3) / K)
+                          ** (1 / m) - outflow[i - 1])
+            s[i - 1] = s[i] - T * (k1 + 2 * k2 + 2 * k3 + k4) / 6
+            inflow[i - 1] = (
+                1 / X * (s[i - 1] / K) ** (1 /
+                                           m) - (1 - X) / X * outflow[i - 1]
+            )
+        return inflow
